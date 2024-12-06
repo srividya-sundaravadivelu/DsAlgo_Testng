20f910da038eb5f6efc03ccef9ae4da2ebd4a532
kekaneAnjali <anjali.kekane@gmail.com>
2024-12-06 10:24:07 -0600

    Stack page  and name page  implementation

--- src/test/java/pages/StackPage.java
+++ src/test/java/pages/StackPage.java
@@ -1,8 +1,11 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import utils.LogHelper;
 import utils.WebDriverWaitUtility;
 
 public class StackPage extends BasePage {
@@ -21,6 +24,17 @@ public class StackPage extends BasePage {
 
 	@FindBy(xpath = "//a[text()='Practice Questions']")
 	private WebElement practiceLink;
+	
+	@FindBy(xpath = "//strong//p[contains(@class,'bg-secondary')]")
+	private WebElement pageHeading;
+	
+	
+	
+	public void clickLinkUnderTopicsCovered(String itemName) {
+		WebElement item = driver
+				.findElement(By.xpath("//a[contains(@class,'list-group-item') and text()='" + itemName + "']"));
+		WebDriverWaitUtility.waitForElementToBeClickable(item).click();
+	}
 
 	public void clickOperationsInStackLink() {
 		WebDriverWaitUtility.waitForElementToBeClickable(operationsInStackLink);
@@ -46,4 +60,16 @@ public class StackPage extends BasePage {
 		WebDriverWaitUtility.waitForElementToBeClickable(practiceLink);
 		practiceLink.click();
 	}
+
+
+	
+	public String getPageHeading() {
+		try {
+			WebDriverWaitUtility.waitForElementToBeVisible(pageHeading);
+			return pageHeading.getText();
+		} catch (TimeoutException e) {
+			LogHelper.error("Page heading element is missing. This indicates a missing functionality.");
+			throw new AssertionError("functionality not implemented", e);
+		}
+}
 }
